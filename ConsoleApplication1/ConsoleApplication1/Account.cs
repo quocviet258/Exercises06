3faacea5c9b08256cabf57575685e2f0957f79ad
Viet <caubeon258@gmail.com>
2019-10-15 15:27:32 +0700

    Exercise06

---
+++ ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Account
+    {
+      public  Account (int amount)
+        {
+
+        }
+        public void deposit(int amount)
+      {
+
+      }
+        public void withDraw(int amount)
+        {
+
+        }
+        public int Balance()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTestAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1Account
+    {
+        
+
+        [TestMethod]
+        public void TestMethodAccount1()
+        {
+             Account acc = new Account(200000);
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account);
+           Assert.AreEqual(200000, acc.Balance());
+        
+        
+            
+        }
+    }
+}
